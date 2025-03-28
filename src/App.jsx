ff755a0f03655479978158b54f11c8d7a4372605
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-28 12:32:14 +0100

    Add film list and display titles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
 import { useState, useEffect } from 'react'
 
+const filmsList = [
+  {
+    title: 'Inception',
+    genre: 'Fantascienza'
+  },
+  {
+    title: 'Il Padrino',
+    genre: 'Thriller'
+  }
+  ,
+  {
+    title: 'Titanic',
+    genre: 'Romantico'
+  },
+  {
+    title: 'Batman',
+    genre: 'Azione'
+  },
+  {
+    title: 'Interstellar',
+    genre: 'Fantascienza'
+  },
+  {
+    title: 'Pulp Fiction',
+    genre: 'Thriller'
+  }
+]
+
+
+
 export default function App() {
 
+  const [films, setFilms] = useState(filmsList)
+
   return (
-    <h1>Ciao</h1>
+
+    <>
+
+      <div className="section">
+        {
+          films.map((film, index) => (
+            <div key={index}>
+              <h1>{film.title}</h1>
+            </div>
+          ))
+        }
+      </div>
+
+
+    </>
+
+
+
   )
 
 }
